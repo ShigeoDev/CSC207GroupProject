535640855c1fa5fd703cea9f594c022cb5f6968a
Kevin Hu <kevinhu01@yahoo.com>
2024-11-11 12:35:24 -0500

    Added files and implemented Login and Sign Up use case

---
+++ src/main/java/interface_adapter/login/LoginPresenter.java
@@ -0,0 +1,43 @@
+package interface_adapter.login;
+
+import interface_adapter.Homepage.HomepageViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.Login.LoginOutputBoundary;
+import use_case.Login.LoginOutputData;
+
+/**
+ * The Presenter for the Login Use Case.
+ */
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final HomepageViewModel homepageViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          HomepageViewModel homepageViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.homepageViewModel = homepageViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
+
+//        final homepageState homepageState = homepageViewModel.getState();
+//        this.homepageViewModel.setState(homepageState);
+//        this.homepageViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(homepageViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        final LoginState loginState = loginViewModel.getState();
+        loginState.setLoginError(error);
+        loginViewModel.firePropertyChanged();
+    }
+}
