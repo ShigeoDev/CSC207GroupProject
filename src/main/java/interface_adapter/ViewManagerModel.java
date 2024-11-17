3423f3bc0691a416b7a90ac86580616a93f36cd1
Nigel <84653162+ShigeoDev@users.noreply.github.com>
2024-11-16 21:13:45 -0500

    change usecase factory to builder

--- src/main/java/interface_adapter/ViewManagerModel.java
+++ src/main/java/interface_adapter/ViewManagerModel.java
@@ -1,29 +1,14 @@
 package interface_adapter;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
+/**
+ * Model for the View Manager. Its state is the name of the View which
+ * is currently active. An initial state of "" is used.
+ */
+public class ViewManagerModel extends ViewModel<String> {
 
-public class ViewManagerModel {
-
-    private String activeViewName;
-
-    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
-
-    public String getActiveView() {
-        return activeViewName;
+    public ViewManagerModel() {
+        super("view manager");
+        this.setState("");
     }
 
-    public void setActiveView(String activeView) {
-        this.activeViewName = activeView;
-    }
-
-    // This is what the Signup Presenter will call to let the ViewModel know
-    // to alert the View
-    public void firePropertyChanged() {
-        support.firePropertyChange("view", null, this.activeViewName);
-    }
-
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        support.addPropertyChangeListener(listener);
-    }
 }
