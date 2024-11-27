8f40c700ee321ed6d3d78b52925c991d4f4911de
chs- <braddy1217@gmail.com>
2024-11-26 21:37:19 -0500

    Added JavaDocs for GetCalories Use case

--- src/main/java/use_case/GetCalories/GetCaloriesInputData.java
+++ src/main/java/use_case/GetCalories/GetCaloriesInputData.java
@@ -1,18 +1,35 @@
 package use_case.GetCalories;
 
+/**
+ * Class represents the input for the Get Calories use case.
+ * Contains necessary information to look up the calories.
+ */
 public class GetCaloriesInputData {
     private final String recipeName;
     private final String username;
 
+    /**
+     * Constructor for the input data.
+     * @param recipeName Name of the recipe to look up
+     * @param username Name of the user making the request
+     */
     public GetCaloriesInputData(String recipeName, String username) {
         this.recipeName = recipeName;
         this.username = username;
     }
 
+    /**
+     * Getter for the recipe name.
+     * @return Name of recipe
+     */
     public String getRecipeName() {
         return recipeName;
     }
 
+    /**
+     * Getter for the username.
+     * @return Name of user requesting information
+     */
     public String getUsername() {
         return username;
     }
